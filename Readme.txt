6a598b4e2a5f390c42011e6c93187d3715df59d6
thomas <warhamerchaos@hotmail.fr>
2016-03-16 18:39:51 +0100

    Test

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Commande pour lancer Akinator
+scala -cp out/production/Akinator:. Akinator
+Commande pour les Tests:
+scala -cp lib/junit-4.12.jar:lib/scalatest_2.11-2.2.0.jar:out/production/Akinator:. org.scalatest.run AkinatorTest
--- src/AkinatorTest.scala
+++ src/AkinatorTest.scala
@@ -5,15 +5,25 @@
 
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner;
+import org.scalatest.junit.JUnitRunner
 
 import Akinator._
 
 @RunWith(classOf[JUnitRunner])
 class AkinatorTest extends FunSuite{
 
-  test("un test"){
-    assert(1 + 1 == 2)
+  test("Test fichierToABanimal"){
+    val abr = fichierToABanimal("base.txt")
+    abr match {
+      case Question(q1,Question(q2,Animal(a1),Animal(a2)),Animal(a3)) => {
+        assert(q1.equals("Est-ce qu'il a des ailes ?"))
+        assert(q2.equals("Est-ce qu'il a des plumes ?"))
+        assert(a1.equals("Pélican"))
+        assert(a2.equals("Chauve-Souris"))
+        assert(a3.equals("Chien"))
+      }
+      case _ => assert(false)
+    }
   }
 
 }
